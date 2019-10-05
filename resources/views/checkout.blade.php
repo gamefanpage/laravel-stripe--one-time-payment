2902be362f992c00c47fd7782b1e1c183dc249da
Creed06 <game.fanpage@gmail.com>
2019-10-05 11:36:30 +0800

    Added Checkout in Navbar

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -40,8 +40,13 @@
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/checkout') }}">{{ __('Checkout') }}</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
+
+
                         @if (Route::has('register'))
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ url('/checkout') }}">Checkout</a>
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
